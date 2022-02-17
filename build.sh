e333d536a7326edf45a6470cd69c698e79aab560
Jakob Schikowski <jakob.schikowski@gmx.de>
2022-02-18 00:28:28 +0100

    Add build.sh

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+
+echo "STEP wtfisjet: cargo fmt"
+cargo fmt
+
+echo "STEP wtfisjet: cargo build"
+cargo build
+
+echo "STEP wtfisjet: cargo build --no-default-features"
+cargo build --no-default-features
+
+echo "STEP wtfisjet: cargo build --all-features"
+cargo build --all-features
+
+echo "STEP wtfisjet: cargo test -- --format=terse"
+cargo test -- --format=terse
+
+echo "STEP wtfisjet: cargo clippy"
+cargo clippy
+
+echo "STEP wtfisjet: cargo doc --no-deps"
+cargo doc --no-deps
+
+echo "STEP wtfisjet: cargo readme > README.md"
+cargo readme > README.md
