e1a535923e914a41c0c795c7106ef8622d76d2b3
Jakob Schikowski <jakob.schikowski@gmx.de>
2022-02-19 13:24:53 +0100

    Add more constructors to `Jet`

--- src/jet.rs
+++ src/jet.rs
@@ -6,13 +6,21 @@ use crate::{Infinitesimal, Number};
 /// - A real part that represents the result of a function.
 /// - An infinitesimal part that represents the exact derivative for each input of the function.
 ///
-/// If you want to write functions with jets, you need to be careful. There are some pitfalls:
+/// # Using a jet based function
+///
+/// If you want to use a jet based function and calculate the derivatives with respect to
+/// some of its inputs, you probably want to use [`Jet::variable`] to initialize these inputs.
+/// Use a different index for each input.
+///
+/// # Writing a jet based function
+///
+/// If you want to write a jet based function, you need to be careful. There are some pitfalls:
 /// - Jets do **not** fulfil the properties of a mathematical field. Most notably there are
 /// multiple jets without an inverse (all jets with zero in their real part). You need to
 /// consider this if you divide jets.
+/// - Continuously differentiable functions are very nice for algorithms like Newton's method.
+/// Unfortunately you can easily lose this property, e.g. by using if/else.
 /// - There is no obvious order for jets.
-/// - The derivatives are only plausible if your function is continuously differentiable.
-/// You can easily lose this property, e.g. by using if/else.
 #[derive(Debug, Clone, PartialEq)]
 pub struct Jet<N: Number, I: Infinitesimal<N>> {
     /// The real part of the jet.
@@ -22,11 +30,33 @@ pub struct Jet<N: Number, I: Infinitesimal<N>> {
 }
 
 impl<N: Number, I: Infinitesimal<N>> Jet<N, I> {
-    /// Creates a new instance with the given real and infinitesimal parts.
+    /// Creates an instance with the given real and infinitesimal parts.
     pub fn new(real: N, infinitesimal: I) -> Self {
         Self {
             real,
             infinitesimal,
+        }
+    }
+
+    /// Creates an instance with the given real part and zeros for the infinitesimal part.
+    ///
+    /// This represents a jet that has no dependencies to any variables.
+    pub fn constant(real: N, dim: usize) -> Self {
+        Self {
+            real,
+            infinitesimal: I::zeros(dim),
+        }
+    }
+
+    /// Creates an instance with the given real part and a single one for the given dimension
+    /// in the infinitesimal part.
+    ///
+    /// Useful for initializing the inputs of the function for which you want to calculate the
+    /// derivatives. Use a different index for each input.
+    pub fn variable(real: N, idx: usize, dim: usize) -> Self {
+        Self {
+            real,
+            infinitesimal: I::one(idx, dim),
         }
     }
 }
