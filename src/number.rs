7b502335b8933431d9f9c35a895c6d9374f38feb
Jakob Schikowski <jakob.schikowski@gmx.de>
2022-02-20 22:29:17 +0100

    Add trait bound `Neg` to `Number`

--- src/number.rs
+++ src/number.rs
@@ -1,10 +1,16 @@
 use std::fmt::Debug;
+use std::ops::Neg;
 
 /// The number type used by [`Jet`](crate::Jet).
 ///
 /// Usually it's float like type, e.g. `f32`.
 pub trait Number:
-    num_traits::NumAssign + num_traits::Inv<Output = Self> + Debug + Clone + PartialOrd
+    num_traits::NumAssign
+    + num_traits::Inv<Output = Self>
+    + Neg<Output = Self>
+    + Debug
+    + Clone
+    + PartialOrd
 {
 }
 
