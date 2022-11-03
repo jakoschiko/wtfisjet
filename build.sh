3a2e00b70c6bbdd95965f9b0d6a02294d15863aa
Jakob Schikowski <jakob.schikowski@gmx.de>
2022-11-04 00:45:56 +0100

    Remove nightly flag from build script

--- build.sh
+++ build.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 set -e
 
-echo "STEP wtfisjet: cargo +nightly fmt"
-cargo +nightly fmt
+echo "STEP wtfisjet: cargo fmt"
+cargo fmt
 
-echo "STEP wtfisjet: cargo +nightly build"
-cargo +nightly build
+echo "STEP wtfisjet: cargo build"
+cargo build
 
-echo "STEP wtfisjet: cargo +nightly build --no-default-features"
-cargo +nightly build --no-default-features
+echo "STEP wtfisjet: cargo build --no-default-features"
+cargo build --no-default-features
 
-echo "STEP wtfisjet: cargo +nightly build --all-features"
-cargo +nightly build --all-features
+echo "STEP wtfisjet: cargo build --all-features"
+cargo build --all-features
 
-echo "STEP wtfisjet: cargo +nightly test -- --format=terse"
-cargo +nightly test -- --format=terse
+echo "STEP wtfisjet: cargo test -- --format=terse"
+cargo test -- --format=terse
 
 echo "STEP wtfisjet: cargo run --example bspline_examples"
 cargo run --example bspline_examples
@@ -22,11 +22,11 @@ cargo run --example bspline_examples
 echo "STEP wtfisjet: cargo run --example curve_fitting"
 cargo run --example curve_fitting
 
-echo "STEP wtfisjet: cargo +nightly clippy"
-cargo +nightly clippy
+echo "STEP wtfisjet: cargo clippy"
+cargo clippy
 
-echo "STEP wtfisjet: cargo +nightly doc --no-deps"
-cargo +nightly doc --no-deps
+echo "STEP wtfisjet: cargo doc --no-deps"
+cargo doc --no-deps
 
 echo "STEP wtfisjet: cargo readme > README.md"
 cargo readme > README.md
